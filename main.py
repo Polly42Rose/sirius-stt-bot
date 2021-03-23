8bfe5756f3e074224d3db0ce669b133031c9cd46
Polly <pdtarantsova@edu.hse.ru>
2021-03-23 19:31:47 +0300

    basic audio save

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import requests
 import subprocess
 import tempfile
 import os
-
-token = '1616165730:AAEUVpUwpefkxn8gxiQtf6HMVRijKOcIyaQ'
+token = ''
 bot = telebot.TeleBot(token)
 
 
@@ -26,36 +25,9 @@ def get_text_messages(message):
 def voice_processing(message):
     file_info = bot.get_file(message.voice.file_id)
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
+    with open("audio.ogg", "wb+") as f:
+        f.write(file.content)
 
-def convert_to_pcm16b16000r(in_filename=None, in_bytes=None):
-    with tempfile.TemporaryFile() as temp_out_file:
-        temp_in_file = None
-        if in_bytes:
-            temp_in_file = tempfile.NamedTemporaryFile(delete=False)
-            temp_in_file.write(in_bytes)
-            in_filename = temp_in_file.name
-            temp_in_file.close()
-        if not in_filename:
-            raise Exception('Neither input file name nor input bytes is specified.')
-
-        # Запрос в командную строку для обращения к FFmpeg
-        command = [
-            r'Project\ffmpeg\bin\ffmpeg.exe',  # путь до ffmpeg.exe
-            '-i', in_filename,
-            '-f', 's16le',
-            '-acodec', 'pcm_s16le',
-            '-ar', '16000',
-            '-'
-        ]
-
-        proc = subprocess.Popen(command, stdout=temp_out_file, stderr=subprocess.DEVNULL)
-        proc.wait()
-
-        if temp_in_file:
-            os.remove(in_filename)
-
-        temp_out_file.seek(0)
-        return temp_out_file.read()
 
 
 bot.polling(none_stop=True)
